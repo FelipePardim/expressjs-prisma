05846a86dd855664ad82cd3b82459419fd21b878
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2023-05-02 14:07:44 -0300

    Adding render deploy cache

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -3,7 +3,9 @@ import IORedis from 'ioredis'
 const redis = new IORedis({
  host: process.env.REDIS_HOST,
  port: Number(process.env.REDIS_PORT),
- password: process.env.REDIS_PASSWORD
+ username: process.env.REDIS_SERVICE_NAME, // Render Redis name, red-xxxxxxxxxxxxxxxxxxxx
+ password: process.env.REDIS_PASSWORD,     // Provided password
+ // tls: true // TLS required when externally connecting to Render Redis
 })
 
 export default redis;
